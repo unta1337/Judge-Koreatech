b80bf7efa9b92c74f123d24ce86d748a421d0b3f
unta1337 <firstapple42@gmail.com>
2021-06-27 05:08:58 +0900

    1008 finding repeating algorithm done

--- C/1008.c
+++ C/1008.c
@@ -1,43 +1,59 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int pow(int i, int n)
-{
-    int result = 1;
-    for (int j = 0; j < n; j++)
-        result *= i;
-
-    return result;
-}
-
 // get 1st to length-th decimal place number.
-int getDecimalPointLength(double decimal, int length)
+long long getDecimalPointLength(double decimal, int length)
 {
-    decimal -= (int)decimal;
-    decimal *= pow(10, length);
+    decimal -= (long long)decimal;
+    for (int i = 0; i < length; i++)
+        decimal *= 10;
 
-    return (int)decimal;
+    return (long long)decimal;
 }
 
 // get start-th to end-th decimal place number.
 int getDecimalPoint(double decimal, int start, int end)
 {
-    int startInt = getDecimalPointLength(decimal, start - 1);
-    int endInt = getDecimalPointLength(decimal, end);
+    long long startInt = getDecimalPointLength(decimal, start - 1);
+    long long endInt = getDecimalPointLength(decimal, end);
 
-    int result = endInt - startInt * pow(10, end - start + 1);
+    long long temp = startInt;
+    for (int i = 0; i < end - start + 1; i++)
+        temp *= 10;
+    int result = endInt - temp;
 
     return result;
 }
 
-bool decimalPointMatch(double decimal, int length)
+int findRepeating(double decimal, int start, int limit, int length, int repeating)
 {
+    int repeatingCount = 0;
+    int current = getDecimalPoint(decimal, start, start + length - 1);
+    int repeatingPart = -1;
+    for (int i = start + length; i < limit; i += length)
+    {
+        int prev = current;
+        current = getDecimalPoint(decimal, i, i + length - 1);
+        if (prev == current)
+        {
+            repeatingCount++;
+            if (repeatingCount >= repeating)
+            {
+                repeatingPart = current;
+                break;
+            }
+        }
+        else
+            break;
+    }
+
+    return repeatingPart;
 }
 
 int main(void)
 {
     // logging stuffs.
-    bool printDecimalPoint = false;
+    bool printDecimalPoint = true;
 
     // input.
     int A, B;
@@ -51,7 +67,11 @@ int main(void)
         printf("%.24lf\n\n", decimal);
     }
 
-    printf("%d\n", getDecimalPoint(decimal, 4, 6));
+    // implementation of finding repeating part.
+    int repeatingPart = findRepeating(decimal, 1, 24, 4, 3);
+
+    // printing result.
+    printf("%d\n", repeatingPart);
 
     return 0;
 }
