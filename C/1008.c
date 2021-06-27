3571dc8eb2cae79b10ad975f6a2787e002336914
unta1337 <firstapple42@gmail.com>
2021-06-27 21:06:39 +0900

    1008 need repeating part search improvement

--- C/1008.c
+++ C/1008.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 // get 1st to length-th decimal place number.
@@ -25,6 +26,7 @@ int getDecimalPoint(double decimal, int start, int end)
     return result;
 }
 
+// find repeating pattern in decimal number.
 int findRepeating(double decimal, int start, int limit, int length, int repeating)
 {
     int repeatingCount = 0;
@@ -50,28 +52,80 @@ int findRepeating(double decimal, int start, int limit, int length, int repeatin
     return repeatingPart;
 }
 
+int findRepeatingPatern(double decimal, int limit, int *start)
+{
+    int repeatingPart = -1;
+    int length;
+    for (length = 1; length <= 4; length++)
+    {
+        for (*start = 1; *start <= limit; *start += length)
+        {
+            int temp = findRepeating(decimal, *start, limit, length, 3);
+            if (temp != -1)
+            {
+                repeatingPart = temp;
+                return repeatingPart;
+            }
+        }
+    }
+
+    return repeatingPart;
+}
+
+void printRepeating(double decimal, int start, int repeatingPart)
+{
+    double decimal_ = decimal;
+    for (int i = 0; i < start - 1; i++)
+        decimal_ *= 10;
+    decimal_ = (int)decimal_;
+    for (int i = 0; i < start - 1; i++)
+        decimal_ /= 10;
+    if (decimal_ == 0)
+        printf("0.");
+    else
+        printf("%g", decimal_);
+    printf("(%d)\n", repeatingPart);
+}
+
 int main(void)
 {
     // logging stuffs.
-    bool printDecimalPoint = true;
+    bool printDecimalPoint = false;
 
     // input.
-    int A, B;
-    scanf("%d %d", &A, &B);
-
-    double decimal = (double)A / B;
-
-    if (printDecimalPoint)
+    int n;
+    scanf("%d", &n);
+    double *decimals = (double*)malloc(n * sizeof(double));
+    int *starts = (int*)malloc(n * sizeof(int));
+    int *repeatingParts = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
     {
-        printf("\nCALCULATED DECIMAL POINT NUMBER:\n");
-        printf("%.24lf\n\n", decimal);
+        int A, B;
+        scanf("%d %d", &A, &B);
+
+        double decimal = (double)A / B;
+        decimals[i] = decimal;
+
+        if (printDecimalPoint)
+        {
+            printf("\nCALCULATED DECIMAL POINT NUMBER:\n");
+            printf("%.24lf\n\n", decimal);
+        }
+
+        // implementation of finding repeating part.
+        int start;
+        int repeatingPart = findRepeatingPatern(decimal, 24, &start);
+        starts[i] = start;
+        repeatingParts[i] = repeatingPart;
     }
 
-    // implementation of finding repeating part.
-    int repeatingPart = findRepeating(decimal, 1, 24, 4, 3);
-
     // printing result.
-    printf("%d\n", repeatingPart);
+    for (int i = 0; i < n; i++)
+        printRepeating(decimals[i], starts[i], repeatingParts[i]);
+
+    free(decimals);
+    free(starts);
+    free(repeatingParts);
 
     return 0;
 }
