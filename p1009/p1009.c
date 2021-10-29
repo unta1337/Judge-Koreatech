b637d01d181a247984520d66f9f142b864033178
unta1337 <firstapple42@gmail.com>
2021-10-30 02:17:52 +0900

    1009번 추가

---
+++ p1009/p1009.c
@@ -0,0 +1,16 @@
+// 1009: Endian
+/*
+ * 빅 엔디안 또는 리틀 엔디안의 데이터가 주어졌을 때, 이를 리틀 엔디언 또는 빅 엔디언으로 변환하여라.
+ */
+// https://judge.koreatech.ac.kr/problem.php?id=1009
+
+// 메모리 사용량: KB
+// 실행 시간: ms
+// 코드 용량: bytes
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
